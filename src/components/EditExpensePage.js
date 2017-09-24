8139830e3c35249fe947912371ed5a2020c19710
rayGreycloud <rayGreycloud@gmail.com>
2017-09-24 12:44:00 -0700

    Grab query value and pass expense id as prop to EditExpensePage

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-  <div>Edit Expense Component</div>
-);
+const EditExpensePage = (props) => {
+  console.log(props)
+  return (<div>Editing the expense with id of {props.match.params.id} Expense Component</div>);
+}
 
 export default EditExpensePage;
