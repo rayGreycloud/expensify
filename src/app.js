2a41226087d9b2d2cceaecb401e54de33e2c8fd6
rayGreycloud <rayGreycloud@gmail.com>
2017-09-24 06:56:28 -0700

    Scaffold header and 404 page

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -19,18 +19,27 @@ const HelpPage = () => (
   <div>Help Component</div>
 );
 const NotFound = () => (
-  <div>404 - this is not the page you're looking for...</div>
-)
+  <div>404 - this is not the page you're looking for...
+    <p><Link to="/">Go home</Link></p>
+  </div>
+);
+
+const Header = () => (
+  <header><h1>Expensify</h1></header>
+);
 
 const routes = (
   <BrowserRouter>
-    <Switch>
-      <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-      <Route path="/create" component={AddExpensePage}/>
-      <Route path="/edit" component={EditExpensePage}/>
-      <Route path="/help" component={HelpPage}/>
-      <Route component={NotFound} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+        <Route path="/create" component={AddExpensePage}/>
+        <Route path="/edit" component={EditExpensePage}/>
+        <Route path="/help" component={HelpPage}/>
+        <Route component={NotFound} />
+      </Switch>
+    </div>
   </BrowserRouter>
 );
 
