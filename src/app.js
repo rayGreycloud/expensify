fe83f2db2e02aeddb38de117b829b069f36e92a3
rayGreycloud <rayGreycloud@gmail.com>
2017-09-24 07:46:40 -0700

    Add navagation links

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Link, NavLink, Route, Switch } from 'react-router-dom';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -25,7 +25,13 @@ const NotFound = () => (
 );
 
 const Header = () => (
-  <header><h1>Expensify</h1></header>
+  <header>
+    <h1>Expensify</h1>
+    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard </NavLink>
+    <NavLink to="/create" activeClassName="is-active">Add Expense</NavLink>
+    <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
+    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+  </header>
 );
 
 const routes = (
