a5a17486139c9d04635983fe49fc1d5c7dcca968
rayGreycloud <rayGreycloud@gmail.com>
2017-10-15 09:18:29 -0700

    Playground - set up filters and actions

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -35,6 +35,14 @@ const setTextFilter = (text = '') => ({
   text
 });
 
+const sortByAmount = () => ({
+  type: 'SORT_BY_AMOUNT'
+});
+
+const sortByDate = () => ({
+  type: 'SORT_BY_DATE'
+});
+
 const expensesReducerDefaultState = [];
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch (action.type) {
@@ -67,7 +75,20 @@ const filtersReducerDefaultState = {
 const filtersReducer = (state = filtersReducerDefaultState, action ) => {
   switch (action.type) {
     case 'SET_TEXT_FILTER':
-      return {...state, text: action.text};
+      return {
+        ...state,
+         text: action.text
+       };
+    case 'SORT_BY_AMOUNT':
+      return {
+        ...state,
+         sortBy: 'amount'
+       };
+    case 'SORT_BY_DATE':
+      return {
+        ...state,
+         sortBy: 'date'
+       };
     default: 
       return state;
   }
@@ -100,6 +121,8 @@ store.dispatch(editExpense(expenseTwo.expense.id, {
 }));
 
 store.dispatch(setTextFilter('rent'));
+store.dispatch(sortByAmount());
+store.dispatch(sortByDate());
 
 const demoState = {
   expenses: [{
