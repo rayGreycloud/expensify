9d946ba6cec510d52765867302e890a7abf56527
rayGreycloud <rayGreycloud@gmail.com>
2017-10-15 09:02:19 -0700

    Playground - use object spread in reducers

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -24,6 +24,17 @@ const removeExpense = ({ id } = {}) => ({
   id
 });
 
+const editExpense = (id, updates) => ({
+  type: 'EDIT_EXPENSE',
+  id,
+  updates  
+});
+
+const setTextFilter = (text = '') => ({
+  type: 'SET_TEXT_FILTER',
+  text
+});
+
 const expensesReducerDefaultState = [];
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch (action.type) {
@@ -31,6 +42,17 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
       return [...state, action.expense];
     case 'REMOVE_EXPENSE':
       return state.filter(({ id }) => id !== action.id );
+    case 'EDIT_EXPENSE':
+      return state.map((expense) => {
+        if (expense.id === action.id) {
+          return {
+            ...expense,
+            ...action.updates
+          };
+        } else {
+          return expense;
+        };
+      });
     default:
       return state;
   }
@@ -44,6 +66,8 @@ const filtersReducerDefaultState = {
 };
 const filtersReducer = (state = filtersReducerDefaultState, action ) => {
   switch (action.type) {
+    case 'SET_TEXT_FILTER':
+      return {...state, text: action.text};
     default: 
       return state;
   }
@@ -70,7 +94,12 @@ const expenseTwo = store.dispatch(addExpense({
 }));
 
 store.dispatch(removeExpense({ id: expenseOne.expense.id }));
-console.log(expenseOne);
+
+store.dispatch(editExpense(expenseTwo.expense.id, {
+  amount: 20000
+}));
+
+store.dispatch(setTextFilter('rent'));
 
 const demoState = {
   expenses: [{
