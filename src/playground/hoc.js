515bf391dfbe72fdbe74306dbb7f4e42ebedd1ba
rayGreycloud <rayGreycloud@gmail.com>
2017-10-16 09:51:10 -0700

    Playground - higher order component demos

---
+++ src/playground/hoc.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Info = (props) => (
+  <div>
+    <h1>Info</h1>
+    <p>This info is: {props.info}</p>
+  </div>
+);
+
+const withAdminWarning = (WrappedComponent) => {
+  return (props) => (
+    <div>
+      { props.isAdmin && <p>HIPAA Warning: This is private info</p>}
+      <WrappedComponent {...props} />
+    </div>
+  );
+};
+
+const requireAuthentication = (WrappedComponent) => {
+  return (props) => (
+    <div>
+      { props.isAuthenticated ? (
+        <WrappedComponent {...props} />
+      ) : (
+        <p>Please sign in first</p>
+      )}
+    </div>
+  );
+};
+
+const AdminInfo = withAdminWarning(Info);
+const AuthInfo = requireAuthentication(Info);
+
+// ReactDOM.render(<AdminInfo isAdmin={true} info="Details, details, details" />, document.getElementById('app'));
+ReactDOM.render(<AuthInfo isAuthenticated={true} info="Details, details, details" />, document.getElementById('app'));
