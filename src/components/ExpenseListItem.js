cbe77b3c2de2a46a00e16f44b3349754fe176b5e
rayGreycloud <rayGreycloud@gmail.com>
2017-10-17 09:15:09 -0700

    Add controlled input to remove expense

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
 
-const ExpenseListItem = ({ description, amount, createdAt}) => (
+const ExpenseListItem = ({ dispatch, description, amount, createdAt, id }) => (
   <div>
     <h4>{description}</h4>
     <p>{amount} - {createdAt}</p>
+    <button onClick={() => {
+      dispatch(removeExpense({ id }));
+    }}
+    >
+      Remove
+    </button>
   </div>
 );
 
-export default ExpenseListItem;
+export default connect()(ExpenseListItem);
