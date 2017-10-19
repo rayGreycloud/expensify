24ba406d24f1e8ae434935c7a68faec95584cbe2
rayGreycloud <rayGreycloud@gmail.com>
2017-10-19 10:10:55 -0700

    Add dynamic Link to edit expense page

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
 import { removeExpense } from '../actions/expenses';
 
 const ExpenseListItem = ({ dispatch, description, amount, createdAt, id }) => (
   <div>
-    <h4>{description}</h4>
+    <Link to={`/edit/${id}`}>
+      <h4>{description}</h4>
+    </Link>
+
     <p>{amount} - {createdAt}</p>
     <button onClick={() => {
       dispatch(removeExpense({ id }));
