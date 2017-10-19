1d4da8c226bfa0eaecbfff0e4a8cc4ce0f99ed53
rayGreycloud <rayGreycloud@gmail.com>
2017-10-19 10:58:36 -0700

    Populate edit expense form with data, dispatch action on submit and push to dashboard

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import ExpenseForm from './ExpenseForm';
+import { editExpense } from '../actions/expenses';
 
 const EditExpensePage = (props) => {
-  console.log(props)
-  return (<div>Editing the expense with id of {props.match.params.id} Expense Component</div>);
-}
+  return (
+    <div>
+      <h1>Edit Expense</h1>
+      <ExpenseForm 
+        expense={props.expense}
+        onSubmit={(expense) => {
+          props.dispatch(editExpense(props.expense.id, expense));
+          props.history.push('/');          
+          console.log('Updated', expense);
+        }}
+      />
+    </div>
+  );
+};
 
-export default EditExpensePage;
+const mapStateToProps = (state, props) => {
+  return {
+    expense: state.expenses.find((expense) => expense.id === props.match.params.id)
+  };
+};
+
+export default connect(mapStateToProps)(EditExpensePage);
