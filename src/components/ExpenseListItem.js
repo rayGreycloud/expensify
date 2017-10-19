b6b3117ff09208fed40c6aa130fa80837f2c8567
rayGreycloud <rayGreycloud@gmail.com>
2017-10-19 11:09:09 -0700

    Move remove expense button to edit expense page and clean up expense list item

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-import { removeExpense } from '../actions/expenses';
-
-const ExpenseListItem = ({ dispatch, description, amount, createdAt, id }) => (
+const ExpenseListItem = ({ description, amount, createdAt, id }) => (
   <div>
     <Link to={`/edit/${id}`}>
       <h4>{description}</h4>
     </Link>
-
     <p>{amount} - {createdAt}</p>
-    <button onClick={() => {
-      dispatch(removeExpense({ id }));
-    }}
-    >
-      Remove
-    </button>
   </div>
 );
 
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
