1330e483c18dc8a426a6ef07c759979d94d06ec6
rayGreycloud <rayGreycloud@gmail.com>
2017-10-20 09:23:27 -0700

    Add test of addExpense action creator with provided values

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,5 +1,27 @@
 import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
 
+test('should setup add expense action object with provided values', () => {
+  const expenseData = {
+    description: 'rent',
+    amount: 109500,
+    createdAt: 1000,
+    note: 'This was last months rent'
+  };
+  const action = addExpense(expenseData);
+  expect(action).toEqual({
+    type: 'ADD_EXPENSE',
+    expense: {
+      ...expenseData,
+      id: expect.any(String)
+    }
+  });
+});
+
+// test('should setup add expense action object with default values', () => {
+
+//   });
+// });
+
 test('should setup remove expense action object', () => {
   const action = removeExpense({ id: '123abc' });
   expect(action).toEqual({
