f54c4cb2fb939ac26dfa2bbfc7f735f5934058c2
rayGreycloud <rayGreycloud@gmail.com>
2017-10-21 10:21:30 -0700

    Add tests of expenses reducer

---
+++ src/tests/reducers/expenses.test.js
@@ -0,0 +1,68 @@
+import expensesReducer from '../../reducers/expenses';
+import expenses from '../fixtures/expenses';
+
+test('should set default state', () => {
+  const state = expensesReducer(undefined, { type: '@@INIT' });
+  expect(state).toEqual([]);
+});
+
+test('should remove expense by id', () => {
+  const action = {
+    type: 'REMOVE_EXPENSE',
+    id: expenses[1].id
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([ expenses[0], expenses[2] ]);
+});
+
+test('should not remove expense if id not found', () => {
+  const action = {
+    type: 'REMOVE_EXPENSE',
+    id: 5
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual(expenses);
+});
+
+test('should add an expense', () => {
+  const newExpense = {
+    description: 'Dog',
+    note: '',
+    amount: 25000,
+    createdAt: 2000    
+  }
+  const action = {
+    type: 'ADD_EXPENSE',
+    expense: newExpense
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([...expenses, newExpense]);
+});
+
+test('should edit expense by id', () => {
+  const updatedNote = 'Updated Note';
+  const action = {
+    type: 'EDIT_EXPENSE',
+    id: expenses[0].id,
+    updates: {
+      note: updatedNote
+    }
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state[0].note).toBe(updatedNote);
+});
+
+test('should not edit expense if id not found', () => {
+  const updatedNote = 'Updated Note';
+  const action = {
+    type: 'EDIT_EXPENSE',
+    id: '5',
+    updates: {
+      note: updatedNote
+    }
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual(expenses);
+});
+
+
