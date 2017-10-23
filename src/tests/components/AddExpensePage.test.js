f9d4d43134124b3bb7f5874a5da1d62a95955fb9
rayGreycloud <rayGreycloud@gmail.com>
2017-10-23 10:09:24 -0700

    Test AddExpensePage render and onSubmit handling

---
+++ src/tests/components/AddExpensePage.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import moment from 'moment';
+
+import { AddExpensePage } from '../../components/AddExpensePage';
+import expenses from '../fixtures/expenses';
+
+test('should render add expense page correctly', () => {
+  const onSubmit = jest.fn();
+  const history = { push: jest.fn() };
+  const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('should handle onSubmit', () => {
+  const onSubmit = jest.fn();
+  const history = { push: jest.fn() };
+  const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
+  wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
+  expect(history.push).toHaveBeenLastCalledWith('/');
+  expect(onSubmit).toHaveBeenLastCalledWith(expenses[1]);
+});
+
