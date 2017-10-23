2966ab6db900d01801a71456260fcbda2abca11a
rayGreycloud <rayGreycloud@gmail.com>
2017-10-23 10:44:03 -0700

    Add beforeEach call and refactor tests

--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -5,17 +5,19 @@ import moment from 'moment';
 import { AddExpensePage } from '../../components/AddExpensePage';
 import expenses from '../fixtures/expenses';
 
+let onSubmit, history, wrapper;
+
+beforeEach(() => {
+  onSubmit = jest.fn();
+  history = { push: jest.fn() };
+  wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);  
+});
+
 test('should render add expense page correctly', () => {
-  const onSubmit = jest.fn();
-  const history = { push: jest.fn() };
-  const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
   expect(wrapper).toMatchSnapshot();
 });
 
 test('should handle onSubmit', () => {
-  const onSubmit = jest.fn();
-  const history = { push: jest.fn() };
-  const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
   wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
   expect(history.push).toHaveBeenLastCalledWith('/');
   expect(onSubmit).toHaveBeenLastCalledWith(expenses[1]);
