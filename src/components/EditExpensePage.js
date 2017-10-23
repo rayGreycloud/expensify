55d2d76baccb43926bc319e8607f7be9bdd9cf5d
rayGreycloud <rayGreycloud@gmail.com>
2017-10-23 11:59:55 -0700

    Refactor EditExpensePage into class component and add named export for testing

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -3,27 +3,33 @@ import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
 import { editExpense, removeExpense } from '../actions/expenses';
 
-const EditExpensePage = (props) => {
-  return (
-    <div>
-      <h1>Edit Expense</h1>
-      <ExpenseForm 
-        expense={props.expense}
-        onSubmit={(expense) => {
-          props.dispatch(editExpense(props.expense.id, expense));
-          props.history.push('/');          
-        }}
-      />
-      <button onClick={() => {
-        props.dispatch(removeExpense({ id: props.expense.id }));
-        props.history.push('/');
-      }}
-      >
-        Remove
-      </button>      
-    </div>
-  );
-};
+export class EditExpensePage extends Component {
+  onSubmit = (expense) => {
+    this.props.dispatch(editExpense(this.props.expense.id, expense));
+    this.props.history.push('/');          
+  }
+  
+  onClick = () => {
+    this.props.dispatch(removeExpense({ id: this.props.expense.id }));
+    this.props.history.push('/');
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>Edit Expense</h1>
+        <ExpenseForm 
+          expense={this.props.expense}
+          onSubmit={this.onSubmit}
+        />
+        <button onClick={this.onClick}
+        >
+          Remove
+        </button>      
+      </div>
+    );    
+  }
+}
 
 const mapStateToProps = (state, props) => {
   return {
