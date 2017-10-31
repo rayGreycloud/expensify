6264f3e7cbe2fedd285b8659d35b45129b4ff729
rayGreycloud <rayGreycloud@gmail.com>
2017-10-30 19:25:55 -0700

    Add auth reducer and tests of auth actions and reducer

--- src/app.js
+++ src/app.js
@@ -4,15 +4,14 @@ import { Provider } from 'react-redux';
 
 import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense, removeExpense, editExpense, startSetExpenses } from './actions/expenses';
-import { setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate } from './actions/filters';
+import { startSetExpenses } from './actions/expenses';
+import { login, logout } from './actions/auth';
 import getVisibleExpenses from './selectors/expenses';
+import { firebase } from './firebase/firebase';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-
-import { firebase } from './firebase/firebase';
 
 const store = configureStore();
 
@@ -34,6 +33,7 @@ ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
+    store.dispatch(login(user.uid));
     store.dispatch(startSetExpenses()).then(() => {
       renderApp();
       if (history.location.pathname === '/') {
@@ -41,6 +41,7 @@ firebase.auth().onAuthStateChanged((user) => {
       }
     });
   } else {
+    store.dispatch(logout());
     renderApp();
     history.push('/');
   }
