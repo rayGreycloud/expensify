1dcf7a8f5e0fdc0c822548ce17d2250f87cdb87c
rayGreycloud <rayGreycloud@gmail.com>
2017-10-31 07:58:35 -0700

    Add PublicPage to LoginPage; Remove HelpPage;

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
+import PublicRoute from '../routers/PublicRoute';
 
 export const LoginPage = ({ startLogin }) => (
   <div>
+    <PublicRoute />
     <button onClick={startLogin}>Login</button>
   </div>
 );
