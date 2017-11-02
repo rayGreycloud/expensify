fb6313c82e22a436f513c8df0d38872311aa77ce
rayGreycloud <rayGreycloud@gmail.com>
2017-11-02 13:36:38 -0700

    Add LoadingPage with spinner and test

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import { startSetExpenses } from './actions/expenses';
 import { login, logout } from './actions/auth';
 import getVisibleExpenses from './selectors/expenses';
 import { firebase } from './firebase/firebase';
+import LoadingPage from './components/LoadingPage';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -29,7 +30,7 @@ const renderApp = () => {
   }
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
