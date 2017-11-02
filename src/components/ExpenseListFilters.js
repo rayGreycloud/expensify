620d41c7052452ee27745cf3a98b8cc01ad115c8
rayGreycloud <rayGreycloud@gmail.com>
2017-11-02 06:46:12 -0700

    Add styles to expense summary inputs

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -36,31 +36,33 @@ export class ExpenseListFilters extends Component {
         <div className="input-group">
           <div className="input-group__item">
             <input 
-            type="text" 
-            value={this.props.filters.text}
-            placeholder="search"
-            onChange={this.onTextChange}
+              type="text"
+              className="text-input" 
+              value={this.props.filters.text}
+              placeholder="Search Expenses"
+              onChange={this.onTextChange}
             />
           </div>
           <div className="input-group__item">
             <select
-            value={this.props.filters.sortBy}
-            onChange={this.onSortChange}
+              className="select"
+              value={this.props.filters.sortBy}
+              onChange={this.onSortChange}
             >
-            <option value="date">Date</option>
-            <option value="amount">Amount</option>
+              <option value="date">Date</option>
+              <option value="amount">Amount</option>
             </select>
           </div>
           <div className="input-group__item">
             <DateRangePicker 
-            startDate={this.props.filters.startDate}
-            endDate={this.props.filters.endDate}
-            onDatesChange={this.onDatesChange}
-            focusedInput={this.state.calendarFocused}  
-            onFocusChange={this.onFocusChange}
-            showClearDates={true}
-            numberOfMonths={1}
-            isOutsideRange={() => false}    
+              startDate={this.props.filters.startDate}
+              endDate={this.props.filters.endDate}
+              onDatesChange={this.onDatesChange}
+              focusedInput={this.state.calendarFocused}  
+              onFocusChange={this.onFocusChange}
+              showClearDates={true}
+              numberOfMonths={1}
+              isOutsideRange={() => false}    
             />
           </div>
         </div>
