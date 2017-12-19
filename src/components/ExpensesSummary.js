1a4e5197aa3411df61ea6321939a450642a74c21
rayGreycloud <rayGreycloud@gmail.com>
2017-12-19 09:34:03 -0800

    Update expense summary to show total number of expenses

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -6,7 +6,7 @@ import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 
-export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+export const ExpensesSummary = ({ expenseCount, visibleExpensesCount, expensesTotal }) => {
   const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
   const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
   
@@ -14,7 +14,8 @@ export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
     <div className="page-header">
       <div className="content-container">
         <h1 className="page-header__title">
-          Viewing <span>{expenseCount}</span> {expenseWord} totaling <span>{formattedExpensesTotal}</span>
+          Viewing <span>{visibleExpensesCount}</span> of <span>{expenseCount}</span> {expenseWord} totaling <span>{formattedExpensesTotal}     </span>
+          {/* <Link className="button button__show-all" to="/dashboard">Show All</Link>                  */}
         </h1>
         <div className="page-header__actions">
           <Link className="button" to="/create">Add Expense</Link>
@@ -27,7 +28,8 @@ const mapStateToProps = (state) => {
   const visibleExpenses = selectExpenses(state.expenses, state.filters);
   
   return {
-    expenseCount: visibleExpenses.length,
+    expenseCount: state.expenses.length,
+    visibleExpensesCount: visibleExpenses.length,
     expensesTotal: selectExpensesTotal(visibleExpenses)
   };
 };
